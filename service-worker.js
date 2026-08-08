24763409fb7da0fe79c2d5a8f088ad939f6c55d0
iglooBK <info@altradingassist.com>
2026-08-08 18:51:03 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'equity-cost-engine-v1';
+const CACHE_NAME = 'equity-cost-engine-v2';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
+  './stock-list.js',
   './icon-192.png',
   './icon-512.png',
   './apple-touch-icon.png'
